f0774ed85943cd6245158be7965b4cbc7696eaee
Rasangana Ariyarathne <115900795+Rasangana@users.noreply.github.com>
2024-12-26 16:02:58 +0530

    Automation1

--- src/main/java/OpenGoogle.java
+++ src/main/java/OpenGoogle.java
@@ -1,5 +1,12 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class OpenGoogle {
     public static void main(String[] args) {
-
+//        System.setProperty("webdriver.chrome.driver","D:\\New folder (4)\\chromedriver-win64\\chromedriver.exe");
+        WebDriver driver= new ChromeDriver();
+        WebDriverManager.chromedriver().setup();
+        driver.get("https://www.google.com/");
     }
 }
